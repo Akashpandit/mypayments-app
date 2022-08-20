9b53f127ebbd6da0c5416ddf59f7ff01b9cbc3ee
Akashpandit <59257474+Akashpandit@users.noreply.github.com>
2022-08-20 19:31:41 +0530

    First commit

--- index.js
+++ index.js
@@ -19,8 +19,11 @@ app.use(express.json());
 
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging') {
     app.use(express.static('client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    });
 }
 
 app.use("/api/payment", paymentRoute);
